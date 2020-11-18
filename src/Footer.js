aaeaa66d938a7a225be8fb7d7611b03c542665e6
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-11-18 11:44:47 -0600

    cards and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2020 Airbnb clone! No rights reserved - this is a demo by FullStackFarzzy!</p>
+      <p>Privacy · Terms · Sitemap · Company Details · Buy Farzzy a Coffee for his hard work!</p>
+    </div>
+  );
+}
+
+export default Footer;
