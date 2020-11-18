311f3616dec3fa6e0618ae33865f88daef8cef86
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-11-18 12:40:51 -0600

    final search

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -7,7 +7,7 @@ function SearchPage() {
   return (
     <div>
       <div className="searchPage__info">
-        <p>62 stays · 26 august to 30 august · 2 guest</p>
+        <p>62 stays · 26 august to 30 august · 2 guests</p>
         <h1>Stays nearby</h1>
         <Button variant="outlined">Cancellation Flexibility</Button>
         <Button variant="outlined">Type of place</Button>
@@ -18,7 +18,7 @@ function SearchPage() {
       <SearchResult
         img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
         location="Private room in center of Houston"
-        title="Stay at this spacious Edwardian House"
+        title="Stay at this spacious EaDo House"
         description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
         star={4.73}
         price="$30 / night"
@@ -28,7 +28,7 @@ function SearchPage() {
       <SearchResult
         img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
         location="Private room in center of Houston"
-        title="Independant luxury studio apartment"
+        title="Independent luxury studio apartment"
         description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
         star={4.3}
         price="$40 / night"
@@ -47,7 +47,7 @@ function SearchPage() {
       <SearchResult
         img="https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
         location="Private room in center of Houston"
-        title="30 mins to Oxford Street, Excel Houston"
+        title="30 mins to Downtown, Houston"
         description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
         star={4.1}
         price="$55 / night"
